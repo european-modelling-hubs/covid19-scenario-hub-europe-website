909052885290150a444183db35afe402b3b6c869
Bisaloo <Bisaloo@users.noreply.github.com>
2022-08-12 15:29:59 +0000

    Deploying to gh-pages from @ covid19-forecast-hub-europe/covid19-scenario-hub-europe-website@3a165ddc03f3b97311235b5139521127ece140ec 🚀

---
+++ code/load/scenarios.R
@@ -0,0 +1,145 @@
+# Set scenario labels
+library(tibble)
+scenarios <- list(
+  "targets" = c("Weekly incident deaths" = "inc death",
+                "Weekly incident cases" = "inc case",
+                "Weekly incident infections" = "inc infection",
+                "Weekly hospital admissions" = "inc hosp",
+                "Weekly critical care admissions" = "inc icu"),
+  # ------------------------------------------------------------------------
+  # Round 0
+  "round-0" = list("round" = 0,
+    "scenario_labels" = c(
+                                       "A-2022-02-25" = "Strong/None",
+                                       "B-2022-02-25" = "Strong/New",
+                                       "C-2022-02-25" = "Weak/None",
+                                       "D-2022-02-25" = "Weak/New"),
+                   "origin_date" = as.Date("2022-03-13"),
+                   "submission_window_end" = as.Date("2022-04-22"),
+                   "scenario_caption" = "Strong/Weak = stronger/weaker immunity maintained over time; \n None/New = immune evading variant introduced May 2022"),
+  # -------------------------------------------------------------------------
+  # Round 1
+  "round-1" = list("round" = 1,
+    "scenario_labels" = c(
+                                      "A-2022-05-22" = "Strong/Summer",
+                                      "B-2022-05-22" = "Strong/Autumn",
+                                      "C-2022-05-22" = "Weak/Summer",
+                                      "D-2022-05-22" = "Weak/Autumn"),
+    "origin_date" = as.Date("2022-05-22"),
+    "submission_window_end" = as.Date("2022-07-01"),
+    "scenario_caption" = "_Guide to scenarios: Stronger or weaker immunity maintained over time ('Strong/Weak'); \n 60+ second booster campaign starting in summer or autumn ('Summer/Autumn')_",
+    "table" = "<table>
+  <tr>
+    <td></td>
+    <td>
+      <b>Slow summer booster campaign</b><br>
+      <ul>
+          <li>2nd booster recommended for 60+</li>
+          <li>Uptake reaches 50% of 1st booster coverage by 15th December</li>
+          <li>Uptake starts <b>15th June*</b></li>
+      </ul>
+    </td>
+    <td>
+      <b>Fast autumn booster campaign</b><br>
+      <ul>
+          <li>2nd** booster recommended for 60+</li>
+          <li>Uptake reaches 50% of 1st booster coverage by 15th December</li>
+          <li>Uptake starts <b>15th September</b></li>
+      </ul>
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <b>Optimistic slow immune waning</b><br>
+      <ul>
+        <li>60% reduction in immunity against infection</li>
+        <li>In <b>8 months</b></li>
+      </ul>
+    </td>
+    <td>
+      Scenario A (<i>'Strong/Summer'</i>)
+    </td>
+    <td>
+      Scenario B (<i>'Strong/Autumn'</i>)
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <b>Pessimistic fast immune waning</b><br>
+      <ul>
+        <li>60% reduction in immunity against infection</li>
+        <li>In <b>3 months</b></li>
+      </ul>
+    </td>
+    <td>
+      Scenario C (<i>'Weak/Summer'</i>)
+    </td>
+    <td>
+      Scenario D (<i>'Weak/Summer'</i>)
+    </td>
+  </tr>
+</table>
+
+_* If a second booster is already offered to 60+ (Greece, Netherlands), vaccination uptake continues as currently_
+
+_** If a second booster is already offered to 60+ (Greece, Netherlands), a third booster dose is recommended_"),
+  # -------------------------------------------------------------------------
+  # Round 2
+  "round-2" = list("round" = 2,
+                   "scenario_labels" = c(
+                     "A-2022-07-24" = "60+Booster/Optimistic",
+                     "B-2022-07-24" = "18+Booster/Optimistic",
+                     "C-2022-07-24" = "60+Booster/Pessimistic",
+                     "D-2022-07-24" = "18+Booster/Pessimistic"),
+                   "origin_date" = as.Date("2022-07-24"),
+                   "submission_window_end" = as.Date("2022-07-29"),
+                   "scenario_caption" = "Guide to scenarios: ",
+                   "table" = "<table>
+  <tr>
+    <td></td>
+    <td>
+      <b>Age 60+ booster campaign</b><br>
+      <ul>
+          <li>2nd* booster recommended for 60+</li>
+          <li>Uptake starts 15th September, and reaches 50% coverage by 15th December</li>
+      </ul>
+    </td>
+    <td>
+      <b>Age 18+ booster campaign</b><br>
+      <ul>
+          <li>2nd* booster recommended for general population, ages 18+</li>
+          <li>Uptake starts 15th September, and reaches 50% coverage by 15th December</li>
+      </ul>
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <b>Optimistic vaccine effectiveness</b><br>
+      <ul>
+        <li>Increased booster vaccine effectiveness to that seen against <b>Delta variant</b></li>
+      </ul>
+    </td>
+    <td>
+      Scenario A
+    </td>
+    <td>
+      Scenario B
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <b>Pessimistic vaccine effectiveness</b><br>
+      <ul>
+        <li>Reduced booster vaccine effectiveness against infection from <b>BA.4/BA.5/BA.2.75 variants</b></li>
+      </ul>
+    </td>
+    <td>
+      Scenario C
+    </td>
+    <td>
+      Scenario D
+    </td>
+  </tr>
+</table>"
+  )
+)
