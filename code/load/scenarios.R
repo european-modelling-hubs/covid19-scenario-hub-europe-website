b10fc44a044281e820cc40af5815c4b141640b67
Rok Grah <Rok.Grah@ecdc.europa.eu>
2023-06-02 09:17:50 +0200

    Updating files for Round 4 report

--- code/load/scenarios.R
+++ code/load/scenarios.R
@@ -9,15 +9,15 @@ scenarios <- list(
   # ------------------------------------------------------------------------
   # Round 0
   "round-0" = list("round" = 0,
-    "scenario_labels" = c(
-                                       "A-2022-02-25" = "Strong/None",
-                                       "B-2022-02-25" = "Strong/New",
-                                       "C-2022-02-25" = "Weak/None",
-                                       "D-2022-02-25" = "Weak/New"),
+                   "scenario_labels" = c(
+                     "A-2022-02-25" = "Strong/None",
+                     "B-2022-02-25" = "Strong/New",
+                     "C-2022-02-25" = "Weak/None",
+                     "D-2022-02-25" = "Weak/New"),
                    "origin_date" = as.Date("2022-03-13"),
                    "submission_window_end" = as.Date("2022-04-22"),
                    "scenario_caption" = "Scenarios: Stronger/weaker immunity maintained over time; \n None/New immune evading variant introduced May 2022",
-    "table" = "<table>
+                   "table" = "<table>
   <tr>
     <td></td>
     <td>
@@ -66,18 +66,18 @@ scenarios <- list(
     </td>
   </tr>
 </table>"),
-  # -------------------------------------------------------------------------
-  # Round 1
-  "round-1" = list("round" = 1,
-    "scenario_labels" = c(
-                                      "A-2022-05-22" = "Strong/Summer",
-                                      "B-2022-05-22" = "Strong/Autumn",
-                                      "C-2022-05-22" = "Weak/Summer",
-                                      "D-2022-05-22" = "Weak/Autumn"),
-    "origin_date" = as.Date("2022-05-22"),
-    "submission_window_end" = as.Date("2022-07-01"),
-    "scenario_caption" = "Scenarios: Stronger or weaker immunity maintained over time ('Strong/Weak'); \n 60+ second booster campaign starting in summer or autumn ('Summer/Autumn')",
-    "table" = "<table>
+# -------------------------------------------------------------------------
+# Round 1
+"round-1" = list("round" = 1,
+                 "scenario_labels" = c(
+                   "A-2022-05-22" = "Strong/Summer",
+                   "B-2022-05-22" = "Strong/Autumn",
+                   "C-2022-05-22" = "Weak/Summer",
+                   "D-2022-05-22" = "Weak/Autumn"),
+                 "origin_date" = as.Date("2022-05-22"),
+                 "submission_window_end" = as.Date("2022-07-01"),
+                 "scenario_caption" = "Scenarios: Stronger or weaker immunity maintained over time ('Strong/Weak'); \n 60+ second booster campaign starting in summer or autumn ('Summer/Autumn')",
+                 "table" = "<table>
   <tr>
     <td></td>
     <td>
@@ -132,20 +132,20 @@ scenarios <- list(
 _* If a second booster is already offered to 60+ (Greece, Netherlands), vaccination uptake continues as currently_
 
 _** If a second booster is already offered to 60+ (Greece, Netherlands), a third booster dose is recommended_"),
-  # -------------------------------------------------------------------------
-  # Round 2
-  "round-2" = list("round" = 2,
-                   "scenario_labels" = c(
-                     # Short text label for scenarios in format:
-                     #  "Policy scenario / Epidemiological scenario"
-                     "A-2022-07-24" = "60+Booster/Optimistic VE",
-                     "B-2022-07-24" = "18+Booster/Optimistic VE",
-                     "C-2022-07-24" = "60+Booster/Pessimistic VE",
-                     "D-2022-07-24" = "18+Booster/Pessimistic VE"),
-                   "origin_date" = as.Date("2022-07-24"),
-                   "submission_window_end" = as.Date("2022-07-29"),
-                   "scenario_caption" = "Scenarios: Autumn second booster campaign among population aged '18+' or '60+'; \n Vaccine effectiveness is 'optimistic'(effectiveness as of a booster vaccine against Delta) or 'pessimistic' (as against BA.4/BA.5/BA.2.75)",
-                   "table" = "<table>
+# -------------------------------------------------------------------------
+# Round 2
+"round-2" = list("round" = 2,
+                 "scenario_labels" = c(
+                   # Short text label for scenarios in format:
+                   #  "Policy scenario / Epidemiological scenario"
+                   "A-2022-07-24" = "60+Booster/Optimistic VE",
+                   "B-2022-07-24" = "18+Booster/Optimistic VE",
+                   "C-2022-07-24" = "60+Booster/Pessimistic VE",
+                   "D-2022-07-24" = "18+Booster/Pessimistic VE"),
+                 "origin_date" = as.Date("2022-07-24"),
+                 "submission_window_end" = as.Date("2022-07-29"),
+                 "scenario_caption" = "Scenarios: Autumn second booster campaign among population aged '18+' or '60+'; \n Vaccine effectiveness is 'optimistic'(effectiveness as of a booster vaccine against Delta) or 'pessimistic' (as against BA.4/BA.5/BA.2.75)",
+                 "table" = "<table>
   <tr>
     <td></td>
     <td>
@@ -192,27 +192,27 @@ _** If a second booster is already offered to 60+ (Greece, Netherlands), a third
     </td>
   </tr>
 </table>"
-  ),
-  # -------------------------------------------------------------------------
-  # Round 3
-  "round-3" = list("round" = 3,
-                   # Short text label for scenarios in format:
-                   #  "Policy scenario / Epidemiological scenario"
-                   "scenario_labels" = c(
-                     "A" = "A. No vaccination, optimistic variants",
-                     "B" = "B. Annual vaccination, optimistic variants",
-                     "C" = "C. Biannual vaccination, optimistic variants",
-                     "D" = "D. No vaccination, pessimistic variants",
-                     "E" = "E. Annual booster campaign, pessimistic variants",
-                     "F" = "F. Biannual booster campaign, pessimistic variants"
-                     ),
-                   # Key dates
-                   "origin_date" = as.Date("2022-09-11"),
-                   "submission_window_end" = as.Date("2022-09-16"),
-                   # Short scenario guide (for website + figure captions)
-                   "scenario_caption" = "Scenarios: New vaccination (none/annual/biannual); \n New variants (optimistic/pessimistic)",
-                   # HTML format scenario table
-                   "table" = "<table>
+),
+# -------------------------------------------------------------------------
+# Round 3
+"round-3" = list("round" = 3,
+                 # Short text label for scenarios in format:
+                 #  "Policy scenario / Epidemiological scenario"
+                 "scenario_labels" = c(
+                   "A" = "A. No vaccination, optimistic variants",
+                   "B" = "B. Annual vaccination, optimistic variants",
+                   "C" = "C. Biannual vaccination, optimistic variants",
+                   "D" = "D. No vaccination, pessimistic variants",
+                   "E" = "E. Annual booster campaign, pessimistic variants",
+                   "F" = "F. Biannual booster campaign, pessimistic variants"
+                 ),
+                 # Key dates
+                 "origin_date" = as.Date("2022-09-11"),
+                 "submission_window_end" = as.Date("2022-09-16"),
+                 # Short scenario guide (for website + figure captions)
+                 "scenario_caption" = "Scenarios: New vaccination (none/annual/biannual); \n New variants (optimistic/pessimistic)",
+                 # HTML format scenario table
+                 "table" = "<table>
                    <tr>
       <td>
       </td>
@@ -285,6 +285,74 @@ _** If a second booster is already offered to 60+ (Greece, Netherlands), a third
   _* Vaccination as planned = Primary schedule + 2 booster doses_
   \n
   _** Seasonal vaccination campaigns: Autumn = 15 September to 15 December; Spring = 15 March to 15 June_"
-  )
+),
+# -------------------------------------------------------------------------
+# Round 4
+"round-4" = list("round" = 4,
+                 # Short text label for scenarios in format:
+                 #  "Policy scenario / Epidemiological scenario"
+                 "scenario_labels" = c(
+                   "A" = "A. Optimistic waning and no new variant",
+                   "B" = "B. Optimistic waning and a new variant",
+                   "C" = "C. Pessimistic waning and no new variant",
+                   "D" = "D. Pessimistic waning and a new variant",
+                 ),
+                 # Key dates
+                 "origin_date" = as.Date("2023-03-15"),
+                 "submission_window_end" = as.Date("2023-03-17"),
+                 # Short scenario guide (for website + figure captions)
+                 "scenario_caption" = "Scenarios: Immunity waning (optimistic/pessimistic); \n New variants (no/yes)",
+                 # HTML format scenario table
+                 "table" = "<table>
+
+<tr>
+    <td>
+    </td>
+    <td>
+      <b> No new variant </b>
+      <ul>
+          <li> Projections are initialised in agreement with the SARS-CoV-2 variants that are circulating at the start of the projection period</li>
+      </ul>
+    </td>
+    <td>
+      <b> New variant (sub-lineage) X </b>
+      <ul>
+          <li> Introduction: From <b>1st May 2023</b>, at least of <b>0.1% of all new infections</b> are due to a new variant X over 16 weeks </li>
+          <li> Characteristics: Variant X has <b>30% immune escape against infection and 0% immune escape against severe outcomes</b>, and the same intrinsic transmissibility and severity as currently dominating Omicron sub-lineage</li>
+      </ul>
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <b>Optimistic waning</b><br>
+      <ul>
+        <li> Protection against infection: <b>6 months</b> median time to transition to <b>70%</b> of the initial immunity </li>
+        <li> Protection against severe outcomes: no waning </li>
+      </ul>
+    </td>
+    <td>
+      Scenario A
+    </td>
+    <td>
+      Scenario B
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <b>Pessimistic waning</b><br>
+      <ul>
+        <li> Protection against infection: <b>6 months</b> median time to transition to <b>40%</b> of the initial immunity </li>
+        <li> Protection against severe outcomes: <b>6 months</b> median time to transition to <b>80%</b> of the initial immunity </li> </li>
+      </ul>
+    </td>
+    <td>
+      Scenario C
+    </td>
+    <td>
+      Scenario D
+    </td>
+  </tr>
+  </table>"
+)
 )
 
